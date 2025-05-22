3e766efd4348f4bf2c9a9f1cd2e289d606ab86bf
philipmathen <philimat12@gmail.com>
2025-05-22 17:10:00 +0200

    inital commit with pointers

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/philipmathen/todo
+
+go 1.23.2
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	var tasks = []string{
+		"Syntax lernen",
+		"Webservice erstellen",
+		"Job finden",
+	}
+
+	fmt.Println("Initial task list:")
+	printTasksToConsole(tasks)
+	addTask(&tasks, "nicht wichsen")
+	printTasksToConsole(tasks)
+
+}
+
+func printTasksToConsole(tasks []string) {
+	for i, task := range tasks {
+		fmt.Printf("Task %d: %s\n", i+1, task)
+	}
+}
+
+func addTask(tasks *[]string, task string) {
+	*tasks = append(*tasks, task)
+	fmt.Printf("\nTask <%s> added to the list\n\n", task)
+}
