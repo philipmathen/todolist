5c3a74622878264fdb8bce3307c200d21dc26064
philipmathen <philimat12@gmail.com>
2025-05-22 22:12:40 +0200

    working todolist app with list stored in ram

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 )
 
 type todoItem struct {
+	Id   string
 	Task string
 	Done bool
 }
@@ -15,16 +17,20 @@ type todoList struct {
 	Items []todoItem
 }
 
-var todos = todoList{
+var MyTodoList = todoList{
 	Items: []todoItem{
-		{Task: "Learn Go", Done: true},
-		{Task: "Build a web app", Done: false},
-		{Task: "Deploy to production", Done: false},
+		{Id: "1", Task: "Learn Go", Done: true},
+		{Id: "2", Task: "Build a web app", Done: false},
+		{Id: "3", Task: "Deploy to production", Done: false},
 	},
 }
 
 func main() {
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/toggle", toggleHandler)
+	http.HandleFunc("/add", addHandler)
+	http.HandleFunc("/delete", deleteHandler)
+	http.HandleFunc("/update", updateHandler)
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
@@ -32,8 +38,58 @@ func main() {
 	}
 }
 
+// indexHandler serves the index page
+// It renders the template and passes the todo list to it
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	rendertemplate(w, "index.html")
+}
+
+// toggleHandler toggles the done status of a task
+func toggleHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	taskId := r.FormValue("id")
+	for index, item := range MyTodoList.Items {
+		if item.Id == taskId {
+			MyTodoList.Items[index].Done = !MyTodoList.Items[index].Done
+			log.Printf("Task < %s > toggled\n", item.Task)
+		}
+	}
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+// addHandler adds a new task to the todo list
+func addHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	newTask := r.FormValue("task")
+	newId := fmt.Sprintf("%d", len(MyTodoList.Items)+1)
+	MyTodoList.Items = append(MyTodoList.Items, todoItem{Id: newId, Task: newTask, Done: false})
+	log.Printf("Task <%s> added to the list\n", newTask)
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func deleteHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	taskId := r.FormValue("id")
+	for index, item := range MyTodoList.Items {
+		if item.Id == taskId {
+			MyTodoList.Items = append(MyTodoList.Items[:index], MyTodoList.Items[index+1:]...)
+			log.Printf("Task <%s> deleted from the list\n", item.Task)
+		}
+	}
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func updateHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	taskId := r.FormValue("id")
+	newTask := r.FormValue("task")
+	for index, item := range MyTodoList.Items {
+		if item.Id == taskId {
+			MyTodoList.Items[index].Task = newTask
+			log.Printf("Task <%s> updated to <%s>\n", item.Task, newTask)
+		}
+	}
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func rendertemplate(w http.ResponseWriter, tmpl string) {
@@ -42,6 +98,6 @@ func rendertemplate(w http.ResponseWriter, tmpl string) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	t.Execute(w, todos)
+	t.Execute(w, MyTodoList)
 
 }
