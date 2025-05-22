b34066789c8aa559acd1d28d37cc9257edcdbd35
philipmathen <philimat12@gmail.com>
2025-05-22 23:54:19 +0200

    bug fixed of site crashing after index conflict when deleting and updating

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"html/template"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 type todoItem struct {
-	Id   string
-	Task string
-	Done bool
+	Number int
+	Task   string
+	Done   bool
 }
 
 type todoList struct {
@@ -19,9 +20,9 @@ type todoList struct {
 
 var MyTodoList = todoList{
 	Items: []todoItem{
-		{Id: "1", Task: "Learn Go", Done: true},
-		{Id: "2", Task: "Build a web app", Done: false},
-		{Id: "3", Task: "Deploy to production", Done: false},
+		{Number: 1, Task: "Learn Go", Done: true},
+		{Number: 2, Task: "Build a web app", Done: false},
+		{Number: 3, Task: "Deploy to production", Done: false},
 	},
 }
 
@@ -41,15 +42,24 @@ func main() {
 // indexHandler serves the index page
 // It renders the template and passes the todo list to it
 func indexHandler(w http.ResponseWriter, r *http.Request) {
+	jsonBytes, err := json.MarshalIndent(MyTodoList, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(string(jsonBytes))
 	rendertemplate(w, "index.html")
 }
 
 // toggleHandler toggles the done status of a task
 func toggleHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
-	taskId := r.FormValue("id")
+	log.Println(r.Form)
+	taskNumber, err := strconv.Atoi(r.FormValue("number"))
+	if err != nil {
+		log.Fatalf("Error converting task number: %v", err)
+	}
 	for index, item := range MyTodoList.Items {
-		if item.Id == taskId {
+		if item.Number == taskNumber {
 			MyTodoList.Items[index].Done = !MyTodoList.Items[index].Done
 			log.Printf("Task < %s > toggled\n", item.Task)
 		}
@@ -59,37 +69,52 @@ func toggleHandler(w http.ResponseWriter, r *http.Request) {
 
 // addHandler adds a new task to the todo list
 func addHandler(w http.ResponseWriter, r *http.Request) {
+	reorderTasks()
 	r.ParseForm()
 	newTask := r.FormValue("task")
-	newId := fmt.Sprintf("%d", len(MyTodoList.Items)+1)
-	MyTodoList.Items = append(MyTodoList.Items, todoItem{Id: newId, Task: newTask, Done: false})
-	log.Printf("Task <%s> added to the list\n", newTask)
+	newNumber := len(MyTodoList.Items) + 1
+	MyTodoList.Items = append(MyTodoList.Items, todoItem{Number: newNumber, Task: newTask, Done: false})
+	log.Printf("Task < %s > added to the list\n", newTask)
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func deleteHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
-	taskId := r.FormValue("id")
+	log.Println(r.Form)
+	taskNumber, err := strconv.Atoi(r.FormValue("number"))
+	if err != nil {
+		log.Fatalf("Error converting task number on delete: %v", err)
+	}
 	for index, item := range MyTodoList.Items {
-		if item.Id == taskId {
+		if item.Number == taskNumber {
 			MyTodoList.Items = append(MyTodoList.Items[:index], MyTodoList.Items[index+1:]...)
-			log.Printf("Task <%s> deleted from the list\n", item.Task)
+			log.Printf("Task < %s > deleted from the list\n", item.Task)
 		}
 	}
+	reorderTasks()
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func updateHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
-	taskId := r.FormValue("id")
+	taskNumber, err := strconv.Atoi(r.FormValue("number"))
+	if err != nil {
+		log.Fatalf("Error converting task number: %v", err)
+	}
 	newTask := r.FormValue("task")
 	for index, item := range MyTodoList.Items {
-		if item.Id == taskId {
+		if item.Number == taskNumber {
 			MyTodoList.Items[index].Task = newTask
-			log.Printf("Task <%s> updated to <%s>\n", item.Task, newTask)
+			log.Printf("Task < %s > updated to < %s >\n", item.Task, newTask)
 		}
 	}
 	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func reorderTasks() {
+	for index, _ := range MyTodoList.Items {
+		MyTodoList.Items[index].Number = index + 1
+	}
 }
 
 func rendertemplate(w http.ResponseWriter, tmpl string) {
